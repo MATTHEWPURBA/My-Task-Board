e6225e19e2305f850adf176ac92ad090079262aa
Robherto Matthew <Robhertomatt@gmail.com>
2025-04-07 12:42:05 +0700

    perubahan di lib aja

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,18 +1,8 @@
 // src/lib/db.ts
 import { PrismaClient } from '@prisma/client';
 
-// Prevent multiple instances of Prisma Client in development
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
-
-const prismaClientSingleton = () => {
-  return new PrismaClient({
-    log: ['query', 'error', 'warn'],
-    errorFormat: 'pretty',
-  });
-};
-
-const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+const prisma = new PrismaClient({
+  datasources: { db: { url: process.env.DATABASE_URL } }
+})
 
 export default prisma;
